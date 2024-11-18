3a0899035588362628e9a47d511b0a4be831a222
Lami47 <liamrosenberg4777@gmail.com>
2024-11-18 15:30:33 +0200

    updates

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,42 +1,42 @@
 import "../Css/Home.css"
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { getFirestore, doc, getDoc } from "firebase/firestore";
-import { getDatabase, ref, get } from "firebase/database";
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import { getAuth, onAuthStateChanged } from "firebase/auth";
+// import { getFirestore, doc, getDoc } from "firebase/firestore";
+// import { getDatabase, ref, get } from "firebase/database";
     
-// Initialize Firestore
-const db = getFirestore(app);
+// // Initialize Firestore
+// const db = getFirestore(app);
 
-// Assuming the user ID is stored in a variable, for example, after the user logs in
-const userId = "user-123";  // You'd typically get this from the authenticated user
+// // Assuming the user ID is stored in a variable, for example, after the user logs in
+// const userId = "user-123";  // You'd typically get this from the authenticated user
 
-// Get the user document from Firestore
-const userDocRef = doc(db, "users", userId);
-const userDocSnapshot = await getDoc(userDocRef);
+// // Get the user document from Firestore
+// const userDocRef = doc(db, "users", userId);
+// const userDocSnapshot = await getDoc(userDocRef);
 
-if (userDocSnapshot.exists()) {
-// If the document exists, get the username
-const username = userDocSnapshot.data().username;
-console.log('Username from Firestore:', username);
-} else {
-console.log('No such user!');
-}
+// if (userDocSnapshot.exists()) {
+// // If the document exists, get the username
+// const username = userDocSnapshot.data().username;
+// console.log('Username from Firestore:', username);
+// } else {
+// console.log('No such user!');
+// }
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBBomfKgw527IwFybOdFjaw7AwIbM4pxKY",
-  authDomain: "myportfolio-f9a26.firebaseapp.com",
-  projectId: "myportfolio-f9a26",
-  storageBucket: "myportfolio-f9a26.firebasestorage.app",
-  messagingSenderId: "1037516038850",
-  appId: "1:1037516038850:web:0d13b5af827f2c5334515b",
-  measurementId: "G-C7Y1HQSWVL"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBBomfKgw527IwFybOdFjaw7AwIbM4pxKY",
+//   authDomain: "myportfolio-f9a26.firebaseapp.com",
+//   projectId: "myportfolio-f9a26",
+//   storageBucket: "myportfolio-f9a26.firebasestorage.app",
+//   messagingSenderId: "1037516038850",
+//   appId: "1:1037516038850:web:0d13b5af827f2c5334515b",
+//   measurementId: "G-C7Y1HQSWVL"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 export default function Home(){
     return(
@@ -53,7 +53,9 @@ export default function Home(){
                         </h3>
                     </div>
                     <div className="BottomOfSideBar">
-                        Username: {usernameSnapshot}
+                        <p>
+                        Username:
+                        </p>
                     </div>
                 </div>
                 <div className="MainScreen">
